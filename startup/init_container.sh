00619febcbcd5341e782358f37222de57e5702c4
DeCato <decato@telenet.be>
2018-08-24 10:02:21 +0200

    Update init_container.sh

--- startup/init_container.sh
+++ startup/init_container.sh
@@ -17,3 +17,6 @@ service ssh start
 
 # Get environment variables to show up in SSH session
 eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+
+#Execute Tika server
+java -jar /tika-server-${TIKA_VERSION}.jar -h 0.0.0.0
